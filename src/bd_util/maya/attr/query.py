0b3ee1caa3d2dd0e6271ae924cef19fabf48e024
AkihiroMamiduka <akihiromamiduka@gmail.com>
2026-04-23 13:46:07 +0900

    feat: アトリビュートの情報一覧を取得する関数追加

--- src/bd_util/maya/attr/query.py
+++ src/bd_util/maya/attr/query.py
@@ -1,18 +1,17 @@
 # coding: utf-8
 
 from enum import Enum
+import dataclasses
+import typing
 
 # maya
 import maya.cmds as cmds
 import maya.api.OpenMaya as om
 
 
-class AttrKind(Enum):
-    ATTRIBUTE_TYPE = 0
-    DATA_TYPE = 1
-
-
-def get_attr_kind(node, attr):
+# get
+#   attr
+def get_attr(node, attr) -> om.MObject:
 
     sel = om.MSelectionList()
     sel.add(node)
@@ -23,9 +22,25 @@ def get_attr_kind(node, attr):
     plug = fn.findPlug(attr, False)
     attr_obj = plug.attribute()
 
-    if attr_obj.hasFn(om.MFn.kTypedAttribute):
-        return AttrKind.DATA_TYPE
+    return attr_obj
 
+
+def is_typed_attr(node, attr):
+    state = False
+    if get_attr(node, attr).hasFn(om.MFn.kTypedAttribute):
+        state = True
+    return state
+
+
+class AttrKind(Enum):
+    ATTRIBUTE_TYPE = 0
+    DATA_TYPE = 1
+
+
+# type kind
+def get_attr_kind(node, attr):
+    if is_typed_attr(node, attr):
+        return AttrKind.DATA_TYPE
     return AttrKind.ATTRIBUTE_TYPE
 
 
@@ -37,6 +52,54 @@ def is_data_type(node, attr):
     return get_attr_kind(node, attr) == AttrKind.DATA_TYPE
 
 
+# data_type_name
+def get_data_type_name(node, attr) -> str | None:
+    attr_obj = get_attr(node, attr)
+
+    if not attr_obj.hasFn(om.MFn.kTypedAttribute):
+        return None
+
+    fn_typed = om.MFnTypedAttribute(attr_obj)
+    data_enum = fn_typed.attrType()
+
+    mapping = {
+        om.MFnData.kString: "string",
+        om.MFnData.kMatrix: "matrix",
+        om.MFnData.kMesh: "mesh",
+        om.MFnData.kNurbsCurve: "nurbsCurve",
+        om.MFnData.kNurbsSurface: "nurbsSurface",
+        om.MFnData.kComponentList: "componentList",
+        om.MFnData.kDoubleArray: "doubleArray",
+        om.MFnData.kIntArray: "intArray",
+        om.MFnData.kVectorArray: "vectorArray",
+        om.MFnData.kPointArray: "pointArray",
+        om.MFnData.kStringArray: "stringArray",
+    }
+
+    return mapping.get(data_enum, str(data_enum))
+
+
+# info
+@dataclasses.dataclass
+class AttrInfo:
+    long_name: str
+    short_name: str
+    attribute_type: str
+    data_type: str
+    default_value: typing.Any
+    min_value: typing.Any
+    max_value: typing.Any
+    soft_min_value: typing.Any
+    soft_max_value: typing.Any
+    enum_name: str
+    multi: bool
+    number_of_children: int
+    parent: str
+    readable: bool
+    writable: bool
+    category: str
+
+
 def safe_query(func, *args, **kwargs):
     """例外が出ても None を返す安全ラッパー"""
     try:
@@ -45,25 +108,24 @@ def safe_query(func, *args, **kwargs):
         return None
 
 
-def print_attribute_info(node_type):
+def get_attribute_infos(node_type) -> list[AttrInfo]:
+    # アトリビュート情報確認用に代理のノードを作成
     node = cmds.createNode(node_type)
 
+    # 全てのアトリビュートを取得
     attrs = cmds.listAttr(node) or []
-    print(attrs)
 
+    attr_infos: list[AttrInfo] = []
     for attr in attrs:
         # long / short name
         long_name = attr
-        short_name = safe_query(
-            cmds.attributeQuery, attr, node=node, shortName=True
-        )
+        short_name = cmds.attributeQuery(attr, node=node, shortName=True)
 
         # attributeType / dataType
-        attribute_type = safe_query(
-            cmds.attributeQuery, attr, node=node, attributeType=True
+        attribute_type = cmds.attributeQuery(
+            attr, node=node, attributeType=True
         )
-
-        is_dataType = is_data_type(node, attr)
+        data_type = get_data_type_name(node, attr)
 
         # default value
         default_value = safe_query(
@@ -74,71 +136,111 @@ def print_attribute_info(node_type):
         min_value = safe_query(
             cmds.attributeQuery, attr, node=node, minimum=True
         )
-
         max_value = safe_query(
             cmds.attributeQuery, attr, node=node, maximum=True
         )
-
         soft_min_value = safe_query(
             cmds.attributeQuery, attr, node=node, softMin=True
         )
-
         soft_max_value = safe_query(
             cmds.attributeQuery, attr, node=node, softMax=True
         )
 
         # enum
-        enum_name = safe_query(
-            cmds.attributeQuery, attr, node=node, listEnum=True
-        )
+        enum_name = cmds.attributeQuery(attr, node=node, listEnum=True)
 
         # multi
-        multi = safe_query(cmds.attributeQuery, attr, node=node, multi=True)
+        multi = cmds.attributeQuery(attr, node=node, multi=True)
 
         # number of children
-        number_of_children = safe_query(
-            cmds.attributeQuery, attr, node=node, numberOfChildren=True
+        number_of_children = cmds.attributeQuery(
+            attr, node=node, numberOfChildren=True
         )
 
         # parent
-        parent = safe_query(
-            cmds.attributeQuery, attr, node=node, listParent=True
-        )
+        parent = cmds.attributeQuery(attr, node=node, listParent=True)
 
         # readable / writable
-        readable = safe_query(
-            cmds.attributeQuery, attr, node=node, readable=True
-        )
-
-        writable = safe_query(
-            cmds.attributeQuery, attr, node=node, writable=True
-        )
+        readable = cmds.attributeQuery(attr, node=node, readable=True)
+        writable = cmds.attributeQuery(attr, node=node, writable=True)
 
         # category
-        category = safe_query(
-            cmds.attributeQuery, attr, node=node, categories=True
-        )
+        category = cmds.attributeQuery(attr, node=node, categories=True)
 
-        # disconnect behaviour
-        disconnect_behaviour = safe_query(
-            cmds.attributeQuery, attr, node=node, disconnectBehavior=True
+        # 情報をまとめる
+        attr_info = AttrInfo(
+            long_name=long_name,
+            short_name=short_name,
+            attribute_type=attribute_type,
+            data_type=data_type,
+            default_value=default_value,
+            min_value=min_value,
+            max_value=max_value,
+            soft_min_value=soft_min_value,
+            soft_max_value=soft_max_value,
+            enum_name=enum_name,
+            multi=multi,
+            number_of_children=number_of_children,
+            parent=parent,
+            readable=readable,
+            writable=writable,
+            category=category,
         )
+        attr_infos.append(attr_info)
 
-        print("--------------------------------------------------------------")
-        print("           longName:", long_name)
-        print("          shortName:", short_name)
-        print("      attributeType:", attribute_type)
-        print("        is_dataType:", is_dataType)
-        print("       defaultValue:", default_value)
-        print("           minValue:", min_value)
-        print("           maxValue:", max_value)
-        print("       softMinValue:", soft_min_value)
-        print("       softMaxValue:", soft_max_value)
-        print("           enumName:", enum_name)
-        print("              multi:", multi)
-        print("   numberOfChildren:", number_of_children)
-        print("             parent:", parent)
-        print("           readable:", readable)
-        print("           writable:", writable)
-        print("           category:", category)
-        print("disconnectBehaviour:", disconnect_behaviour)
+    # ノードを削除
+    cmds.delete(node)
+
+    # 戻り値
+    return attr_infos
+
+
+def print_attribute_infos(node_type, valid_value=True):
+    attr_infos: list[AttrInfo] = get_attribute_infos(node_type)
+    for attr_info in attr_infos:
+
+        # title
+        title = "-" * 8
+        title = "{} {} ({}) ({}) ".format(
+            title,
+            attr_info.long_name,
+            attr_info.attribute_type,
+            attr_info.data_type if attr_info.data_type else "",
+        )
+        title = title.ljust(50, "-")
+        print(f"{title} ")
+
+        # info
+        if valid_value and attr_info.long_name:
+            print("           longName:", attr_info.long_name)
+        if valid_value and attr_info.short_name:
+            print("          shortName:", attr_info.short_name)
+        if valid_value and attr_info.attribute_type:
+            print("      attributeType:", attr_info.attribute_type)
+        if valid_value and attr_info.data_type:
+            print("           dataType:", attr_info.data_type)
+        if valid_value and attr_info.default_value:
+            print("       defaultValue:", attr_info.default_value)
+        if valid_value and attr_info.min_value:
+            print("           minValue:", attr_info.min_value)
+        if valid_value and attr_info.max_value:
+            print("           maxValue:", attr_info.max_value)
+        if valid_value and attr_info.soft_min_value:
+            print("       softMinValue:", attr_info.soft_min_value)
+        if valid_value and attr_info.soft_max_value:
+            print("       softMaxValue:", attr_info.soft_max_value)
+        if valid_value and attr_info.enum_name:
+            print("           enumName:", attr_info.enum_name)
+        if valid_value and attr_info.multi:
+            print("              multi:", attr_info.multi)
+        if valid_value and attr_info.number_of_children:
+            print("   numberOfChildren:", attr_info.number_of_children)
+        if valid_value and attr_info.parent:
+            print("             parent:", attr_info.parent)
+        if valid_value and attr_info.readable:
+            print("           readable:", attr_info.readable)
+        if valid_value and attr_info.writable:
+            print("           writable:", attr_info.writable)
+        if valid_value and attr_info.category:
+            print("           category:", attr_info.category)
+        print("")
