bfd2b89a653c63069ff1e448e422835998715344
AkihiroMamiduka <akihiromamiduka@gmail.com>
2026-04-24 09:02:25 +0900

    fix: DataTypクラス定義時に、Genericとして正しく指定できていなかった不具合を修正

--- src/bd_util/maya/node/operator/attr/dt/_core.py
+++ src/bd_util/maya/node/operator/attr/dt/_core.py
@@ -1,13 +1,19 @@
 # coding: utf-8
 from __future__ import annotations
+from typing import TypeVar, Type, cast
 
+# self
 from .._core import Attr, Plug
 
+A = TypeVar("A", bound="Attr")
 
-class DataTypePlug(Plug["DataTypeAttr"]):
+P = TypeVar("P", bound="Plug")
+
+
+class DataTypePlug(Plug[A]):
     pass
 
 
-class DataTypeAttr(Attr[DataTypePlug]):
+class DataTypeAttr(Attr[P]):
     ATTR_TYPE = "typed"
-    PLUG_CLS = DataTypePlug
+    PLUG_CLS = cast(Type[P], DataTypePlug)
