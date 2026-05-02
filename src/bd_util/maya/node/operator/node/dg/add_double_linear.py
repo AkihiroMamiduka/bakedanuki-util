f98524d09a58064768971d720f579760afed4337
AkihiroMamiduka <akihiromamiduka@gmail.com>
2026-05-02 12:49:44 +0900

    fix: アトリビュートタイプ間違い修正

--- src/bd_util/maya/node/operator/node/dg/add_double_linear.py
+++ src/bd_util/maya/node/operator/node/dg/add_double_linear.py
@@ -1,14 +1,14 @@
 # coding: utf-8
 from ._core import DG
-from ...attr.at.double import DoubleAttr
+from ...attr.at.double_linear import DoubleLinearAttr
 
 
 class AddDoubleLinear(DG):
     NODE_TYPE = "addDoubleLinear"
 
-    input1 = DoubleAttr()
+    input1 = DoubleLinearAttr()
     i1 = input1
-    input2 = DoubleAttr()
+    input2 = DoubleLinearAttr()
     i2 = input2
-    output = DoubleAttr()
+    output = DoubleLinearAttr()
     o = output
