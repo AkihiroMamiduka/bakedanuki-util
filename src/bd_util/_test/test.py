bf8e9b2eb4078b8a3c7e92c898f7fc4f9b55da7f
AkihiroMamiduka <akihiromamiduka@gmail.com>
2026-07-03 14:04:44 +0900

    test: 作業中のテストスクリプトを書く為のテストを追加

---
+++ src/bd_util/_test/test.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+
+from bd_util import NodeCreater
+
+creater = NodeCreater()
+
+cmp_m = creater.compose_matrix(name="cmp_m")
+mult_m = creater.mult_matrix(name="mult_m")
+
+cmp_m.outputMatrix > mult_m.matrixIn[next]
+
+creater.modifier_manager.do_it_dg()
