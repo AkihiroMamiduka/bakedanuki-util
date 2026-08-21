dee695d0838a4baf1853593b37018e020124b985
AkihiroMamiduka <akihiromamiduka@gmail.com>
2026-08-21 13:04:27 +0900

    test: 一時的テスト更新

--- bakedanuki/bakedanuki-util/python/bd_util/_test/test.py
+++ bakedanuki/bakedanuki-util/python/bd_util/_test/test.py
@@ -13,5 +13,11 @@ def main():
     cam = nodes.create.camera(name="camera", parent=transform)
     cam.focalLength.set(100)
 
+    jnt = nodes.create.joint(name="jnt_locator")
+    jnt.drawStyle.set(jnt.drawStyle.NONE)
+    jnt.rotate.set(45, 30, 90)
+    jnt_loc = nodes.create.locator(name=f"{jnt.name}Shape", parent=jnt)
+    jnt_loc.localScale.set(2, 5, 8)
+
     nodes.modifier_manager.do_it_dag()
     nodes.modifier_manager.do_it_dg()
