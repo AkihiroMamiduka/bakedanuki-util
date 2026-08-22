83a6bad0654dadf5a5bdb155c8e32327736e13f4
AkihiroMamiduka <akihiromamiduka@gmail.com>
2026-08-22 15:16:16 +0900

    feat: DAG NodeOperator に children() を追加

--- bakedanuki/bakedanuki-util/python/bd_util/_test/test.py
+++ bakedanuki/bakedanuki-util/python/bd_util/_test/test.py
@@ -14,7 +14,7 @@ def main():
     transform_0 = nodes.create.transform(name="trsf_0")
     joint_0 = nodes.create.joint(name="jnt_0", parent=transform_0)
     transform_1 = nodes.create.transform(name="trsf_1", parent=joint_0)
-    joint_1 = nodes.create.joint(name="jnt_1", parent=transform_1)
+    _ = nodes.create.joint(name="jnt_1", parent=transform_1)
 
     logger.debug(f"joint_0.children(): {joint_0.children()}")
 
@@ -26,9 +26,14 @@ def main():
     for child in children:
         logger.debug(child)
         logger.debug(f"type(child): {type(child)}")
+        child.translate.set(1, 2, 3)
 
     logger.debug(f"transform_1.children(): {transform_1.children()}")
     children = transform_1.children()
     for child in children:
         logger.debug(child)
         logger.debug(f"type(child): {type(child)}")
+        child.translate.set(4, 5, 6)
+        child.jointOrient.set(45, 90, 135)
+
+    nodes.modifier_manager.do_it_dg()
